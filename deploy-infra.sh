571a91dcfdb9aa794e611420b67918700100625f
Emmanuel Naziga <enaziga@Emmanuels-MBP.home>
2020-01-03 23:40:50 -0500

    Create infrastructure via CloudFormation

---
+++ deploy-infra.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+STACK_NAME=awsbootstrap
+REGION=us-east-2
+CLI_PROFILE=awsbootstrap
+EC2_INSTANCE_TYPE=t2.micro
+
+#AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
+#CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploy the CloudFormation template
+echo -e "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+--region $REGION \
+--profile $CLI_PROFILE \
+--stack-name $STACK_NAME \
+--template-file main.yml \
+--no-fail-on-empty-changeset \
+--capabilities CAPABILITY_NAMED_IAM \
+--parameter-overrides \
+EC2InstanceType=$EC2_INSTANCE_TYPE
+
+# If the deploy succeeded, show the DNS name of the created instance
+if [ $? -eq 0 ]; then
+aws cloudformation list-exports \
+--profile awsbootstrap \
+--query "Exports[?Name=='InstanceEndpoint'].Value"
+fi
